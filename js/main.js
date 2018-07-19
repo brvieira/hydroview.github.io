4f62b7d60a74911aeb5c40a4b7fff137a5ec4f40
Bruno Vieira Rosa <bruno.silvavieira@gmail.com>
2018-07-19 16:06:46 -0300

    Ajustes de layout

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@ $(function () {
             $dados.append('<spam>Não existem dados à serem exibidos</spam>');
         }
         else {
-            $dados.append('<tbody>');
+            $dados.append("<thead>");
+            $dados.append("<tr>");
+            $dados.append("<th>Temperatura Interna</th>");
+            $dados.append("<th>Umidade Interna</th>");
+            $dados.append("</tr>");
+            $dados.append("</thead>");
+            $dados.append("<tbody>");
             $.each(data, function (key, value) {
                 $dados.append('<tr><td>' + value.temperaturaInterna + ' °C</td>' +
                               '<td>' + value.umidadeInterna + ' %</td></tr>');
