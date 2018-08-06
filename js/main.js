a42bbb4825947db24a4c3321d851b6a80166433a
Bruno Vieira Rosa <bruno.silvavieira@gmail.com>
2018-08-06 15:26:51 -0300

    Mostrando temperatura da solução

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 $(function () {
-    var $dados = $('#dados');
-
+    var dados = $('#dados');
+    var date;
     $.getJSON('https://hydroapi.herokuapp.com/dados', function (data) {
-        console.log(data);
         if (data.length == 0) {
-            $dados.append('<spam>Não existem dados à serem exibidos</spam>');
+            dados.append('<spam>Não existem dados à serem exibidos</spam>');
         }
         else {
-            $dados.append("<thead>");
-            $dados.append("<tr>");
-            $dados.append("<th>Temperatura Interna</th>");
-            $dados.append("<th>Umidade Interna</th>");
-            $dados.append("</tr>");
-            $dados.append("</thead>");
-            $dados.append("<tbody>");
+            dados.append("<thead>");
+            dados.append("<tr>");
+            dados.append("<th>Data</th>");
+            dados.append("<th>Temperatura Solução</th>");
+            dados.append("<th>Temperatura Interna</th>");
+            dados.append("<th>Umidade Interna</th>");
+            dados.append("</tr>");
+            dados.append("</thead>");
+            dados.append("<tbody>");
             $.each(data, function (key, value) {
-                $dados.append('<tr><td>' + value.temperaturaInterna + ' °C</td>' +
-                              '<td>' + value.umidadeInterna + ' %</td></tr>');
+                date = new Date(value.data);
+
+                dados.append('<tr><td>' + date + '</td><td>' + value.temperaturaSolucao.toFixed(2) + ' °C</td>' +
+                    '<td>' + value.temperaturaInterna + ' °C</td>' +
+                    '<td>' + value.umidadeInterna + ' %</td></tr>');
             });
-            $dados.append('</tbody>');
+            dados.append('</tbody>');
         }
     });
 });
